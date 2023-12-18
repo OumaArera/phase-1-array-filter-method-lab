6d3b458a56a09d87ea10532145db7a48662e5ff1
OumaArera <johnouma999@gmail.com>
2023-12-18 12:10:20 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+const findMatching = (driversName, string) => {
+    let matchingDriver = driversName.filter(driversName => driversName.toLowerCase() === string.toLowerCase());
+    return matchingDriver;
+}
+
+const fuzzyMatch = (driversName, string) => {
+    let nameStartWith = driversName.filter(driversName => driversName.toLowerCase().startsWith(string.toLowerCase()));
+    return nameStartWith;
+}
+
+const matchName = (driversName, string) => {
+    let matchingDriver = driversName.filter(driversName => driversName.name.toLowerCase() === string.toLowerCase());
+    return matchingDriver;
+}
